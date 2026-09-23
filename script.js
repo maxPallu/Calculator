0af3532e57d29940940dd0f5fdb0b9da99c11fe2
maxPallu <32013577+maxPallu@users.noreply.github.com>
2026-09-23 16:11:50 +0200

    Create another version for testing

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-const zero = document.querySelector(".zero");
-zero.addEventListener("click", () => {
-    createText("0");
+const add = document.querySelector(".add");
+add.addEventListener("click", () => {
+    let num1 = prompt("What's your first number?");
+    let num2 = prompt("What's your second number?");
+
+    let result = addNumbers(num1, num2);
+
+    const container = document.querySelector(".container");
+    const div = document.createElement("div");
+    div.classList.add("result");
+    div.innerText = num1+ " + "+ num2 + " = " + result;
+
+    container.appendChild(div);
 });
 
-const one = document.querySelector(".one");
-one.addEventListener("click", () => {
-    createText("1");
-});
-
-const plus = document.querySelector(".plus");
-plus.addEventListener("click", () => {
-    addSign("+");
-})
-
-function createText(text) {
-    const div = document.createElement("div");
-    div.innerText = text;
-    
-    const screen = document.querySelector(".screen");
-    screen.appendChild(div);
-}
-
-function addSign(sign) {
-    const div = document.createElement("div");
-    div.innerText = sign;
-    const signScreen = document.querySelector(".sign");
-    signScreen.appendChild(div);
+function addNumbers(num1, num2) {
+    return +num1 + +num2;
 }
